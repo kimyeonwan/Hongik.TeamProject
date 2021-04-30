6fd83190944821b210c3dc7fa9b01db2ef3c24fe
kimyeonwan <dusdhks7@gmail.com>
2021-04-30 21:31:30 +0900

    메인화면 구현
    에셋과 메인화면 구현해놨습니다.
    INGAME 씬에서 인게임 UI 만드시면
    될거같아요

---
+++ Assets/Script/Button_Manager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Button_Manager : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void StartButton()
+    {
+        SceneManager.LoadScene("INGAME");
+    }
+    public void ExitButton()
+    {
+        Debug.Log("Exit");
+        Application.Quit();
+    }
+
+
+}
